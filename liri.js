a1bca3a8507840734cf9c7a5092cf8c3dff594bb
WagnerAndy <wagner.andrew.james@gmail.com>
2018-12-08 18:44:49 -0700

    Fully functioning before restructure

--- liri.js
+++ liri.js
@@ -4,7 +4,6 @@ var Spotify = require('node-spotify-api');
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require('moment');
-moment().format();
 
 
 // CHOICE WRAPPER START
@@ -32,19 +31,30 @@ inquirer.prompt([
 
             var concert = userConcert.concert;
             console.log("Concert is: " + concert);
-            var concertQueryURL = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp&date=upcoming";
+            var concertQueryURL = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp";
 
             axios.get(concertQueryURL)
                 .then(
-                    function (concertResponse) {
-                        console.log(`Venue: ${concertResponse.data[0].venue.name}\nVenue Location: ${concertResponse.data[0].venue.city}, ${concertResponse.data[0].venue.region}\nDate: ${concertResponse.data[0].datetime}`);
+                    function (response) {
+
+                        var concertResponse = response.data;
+
+                        if (concertResponse.length === 0) {
+                            console.log("There are no concerts scheduled for this artist.");
+
+                        } else {
+
+                            concertResponse.forEach(function (concert) {
+                                console.log(`Venue: ${concert.venue.name}\nVenue Location: ${concert.venue.city}, ${concert.venue.region}\nDate: ${moment(concert.datetime).format("MM/DD/YYYY")}`);
+
+                            })
+                        }
                     }
+
                 );
         });
     }
-// CHOICE IS CONCERT END
-
-
+    // CHOICE IS CONCERT END
 
     // CHOICE IS SONG START
     if (user.choice === "Song") {
@@ -61,20 +71,24 @@ inquirer.prompt([
 
             var spotify = new Spotify(keys.spotify);
             var song = userSong.song;
-            console.log("The song is: " + song);
+
 
             spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
                 if (err) {
                     return console.log('Error occurred: ' + err);
                 }
 
-                console.log(JSON.stringify(data, null, 1));
-                debugger;
+                console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+                console.log(`Song Name: ${data.tracks.items[0].name}`);
+                console.log(`Spotify Link: ${data.tracks.items[0].preview_url}`);
+                console.log(`Album: ${data.tracks.items[0].album.name}`);
+
+
             });
 
         });
     }
-// CHOICE IS SONG END
+    // CHOICE IS SONG END
 
 
     // CHOICE IS MOVIE START
@@ -90,36 +104,27 @@ inquirer.prompt([
 
         ]).then(function (userMovie) {
 
-            var movie = userMovie.movie;
-            // console.log("Movie is: " + movie);
+            var movie;
+
+            if (!userMovie.movie) {
+                movie = "Mr. Nobody";
+            } else {
+                movie = userMovie.movie;
+            };
+
+            console.log("Movie is: " + movie);
             var movieQueryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&y=&plot=short";
 
-            if (movie) {
-                axios.get(movieQueryURL)
-                    .then(
-                        function (movieResponse) {
-                            console.log(`Title: ${movieResponse.data.Title}\nYear: ${movieResponse.data.Year}\nIMDB Rating: ${movieResponse.data.imdbRating}\nRotten Tomatoes Rating: ${movieResponse.data.Ratings[1].Value}\nProduction Country: ${movieResponse.data.Country}\nLanguage: ${movieResponse.data.Language}\nPlot: ${movieResponse.data.Plot}\nActors: ${movieResponse.data.Actors}`);
-                        }
-                    );
-            } else {
-                axios.get("http://www.omdbapi.com/?apikey=trilogy&t=Rainman&y=&plot=short")
-                    .then(
-                        function (movieResponse) {
-                            console.log(`Title: ${movieResponse.data.Title}\nYear: ${movieResponse.data.Year}\nIMDB Rating: ${movieResponse.data.imdbRating}\nRotten Tomatoes Rating: ${movieResponse.data.Ratings[1].Value}\nProduction Country: ${movieResponse.data.Country}\nLanguage: ${movieResponse.data.Language}\nPlot: ${movieResponse.data.Plot}\nActors: ${movieResponse.data.Actors}`);
-                        }
-                    );
-            }
-
+            axios.get(movieQueryURL)
+                .then(
+                    function (movieResponse) {
+                        console.log(`Title: ${movieResponse.data.Title}\nYear: ${movieResponse.data.Year}\nIMDB Rating: ${movieResponse.data.imdbRating}\nRotten Tomatoes Rating: ${movieResponse.data.Ratings[1].Value}\nProduction Country: ${movieResponse.data.Country}\nLanguage: ${movieResponse.data.Language}\nPlot: ${movieResponse.data.Plot}\nActors: ${movieResponse.data.Actors}`);
+                    }
+                );
         });
     }
-// CHOICE IS MOVIE END
-
-
-
+    // CHOICE IS MOVIE END
 
 });
 // CHOICE WRAPPER END
-
-
-
 
